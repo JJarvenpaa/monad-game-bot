b3a4cb0ab40c0be2ab58890f634b75c0ffbe3d51
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-01-23 15:14:00 +0200

    create game with POST request

--- game.js
+++ game.js
@@ -1,9 +1,29 @@
 import fetch from 'node-fetch';
 
-async function getGame() {
-    const response = await fetch('https://koodipahkina.monad.fi/api');
-    const data = await response.json();
-    console.log(data);
+
+//TODO: 1. Create game with POST request
+//      2. Use a secret file with authentication code 
+var gameData = null
+var token = null
+
+async function createGameSession()  {
+
+    const response = await fetch('https://koodipahkina.monad.fi/api/game', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+        },
+    });
+    //TODO: error handling
+    const data = await response.json()
+
+    return data
 }
 
-getGame()
+async function startGame() {
+    gameData = await createGameSession()
+
+}
+
+startGame()
