061dfa9cf05582df83316b2fd17184706d7087e3
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-01-23 15:37:45 +0200

    Read token from env file or throw error

--- game.js
+++ game.js
@@ -3,16 +3,16 @@ import fetch from 'node-fetch';
 
 //TODO: 1. Create game with POST request
 //      2. Use a secret file with authentication code 
-var gameData = null
-var token = null
 
 async function createGameSession()  {
-
+    //TODO: make a specific error
+    if(process.env.TOKEN == undefined) throw new Error("No token found, remember to add it to .env file");
+    
     const response = await fetch('https://koodipahkina.monad.fi/api/game', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token,
+            'Authorization': 'Bearer ' + process.env.TOKEN,
         },
     });
     //TODO: error handling
@@ -22,7 +22,9 @@ async function createGameSession()  {
 }
 
 async function startGame() {
-    gameData = await createGameSession()
+    let gameData = await createGameSession()
+    console.log(gameData.gameId)
+    console.log(gameData.status)
 
 }
 
