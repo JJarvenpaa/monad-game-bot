f8178c8debfd1573f2b799c54c73368a61f25141
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-01-27 17:29:26 +0200

    Check if next card can be added as a set, take it if money

--- game.js
+++ game.js
@@ -21,7 +21,33 @@ async function startGame() {
     let gameData = await createGameSession()
     console.log(gameData.gameId)
     console.log(gameData.status)
+    //I'm always the first player
+    let player = gameData.status.players[0]
+    let money = player.money
+    let cardValue = gameData.status.money
+    let cardsArray = player.cards
 
+    let takeCard = false
+
+    // function to check if card is a set, take it if money
+    if(money >= cardValue && checkSetCard(cardsArray, gameData.status.card)) {
+        takeCard = true;
+    } 
+    
+}
+
+const checkSetCard = (cardsArray, nextCard) =>  {
+    let takeCard = false
+    //check cardsArray and if one of the cards is offset 1 to the current card return true, otherwise false
+    //For testing only 
+    //cardsArray = [25, 1, 3, 4]
+    //nextCard = 2
+    
+    cardsArray.forEach(card => {
+        if(nextCard + 1 == card || nextCard - 1 == card) takeCard = true 
+    });
+
+    return takeCard
 }
 
 startGame()
