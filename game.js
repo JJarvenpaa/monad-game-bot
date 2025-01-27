ff988f8380693fa19a41489686a253a141024c8f
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-01-27 15:19:23 +0200

    Add try catch to api request with timeout

--- game.js
+++ game.js
@@ -4,21 +4,48 @@ async function createGameSession()  {
     //TODO: make a specific error
     if(process.env.TOKEN == undefined) throw new Error("No token found, remember to add it to .env file");
     
-    const response = await fetch('https://koodipahkina.monad.fi/api/game', {
+    try {
+    const response = await fetch('https://koodipahkina.monad.fi/api', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + process.env.TOKEN,
         },
+        signal: AbortSignal.timeout(5000),
     });
-    //TODO: error handling
+    
     const data = await response.json()
 
     return data
+
+    } catch(err) {
+        if (err.name === "TimeoutError") {
+            console.error("Timeout: It took more than 5 seconds to get the result!");
+        }
+
+        else if (err.name === "AbortError") {
+            console.error(
+              "Fetch aborted by user action (browser stop button, closing tab, etc.",
+            )
+        }
+
+        else if (err.name === "TypeError") {
+            console.error("AbortSignal.timeout() method is not supported");
+
+        } else {
+            // A network error, or some other problem.
+            console.error(`Error: type: ${err.name}, message: ${err.message}`);
+        }
+    }
+
+    
 }
 
 async function startGame() {
     let gameData = await createGameSession()
+
+    if(gameData == undefined) return 
+
     console.log(gameData.gameId)
     console.log(gameData.status)
 
