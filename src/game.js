df67d0fdb45a98addd6037dc629feae971ed3c3c
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-01-31 20:40:47 +0200

    Add moneyBuffer to rules and one second sleep before request

--- src/game.js
+++ src/game.js
@@ -10,35 +10,41 @@ async function startGame() {
 async function playRound(gameData, gameId) {
     
     console.log(gameData.status)
+    let takeCard = false
     let player = gameData.status.players.find(({ name }) => name === 'JJarvenpaa')
     let money = player.money
     let cardValue = gameData.status.money
     let cardsArray = player.cards
     let nextCard = gameData.status.card
-    let takeCard = false
 
+    let moneyBuffer = 4 //How much money should be left after taking a card
+    let moneyBufferOnSet = 1 //How much money should be left after taking a set card
+
+    
     if(money == 0) {
         takeCard = true;
         
-    } else if(money >= cardValue && checkSetCard(cardsArray, nextCard)) {
+    } else if(money - moneyBufferOnSet >= cardValue && checkSetCard(cardsArray, nextCard)) {
         takeCard = true;
 
-    } else if(money >= cardValue && nextCard <= 16) {
+    } else if(money - moneyBuffer >= cardValue && nextCard <= 16) {
         //if not set card and has money, take card if card is smaller or equal to than half of the highest card
+        //TODO: maybe this only needs to happen on the first card? It seems that this rule makes us poor
         takeCard = true
         
-    } else if(money >= cardValue && money < 8 && cardValue > 4) {
-        //if money is < 8 and cardValue is > 4, take it to keep money situation good
+    } else if(money - moneyBuffer >= cardValue && money < 8 && cardValue > 4) {
+        //take it to keep money situation good
         takeCard = true
 
     } else {
         takeCard = false
     }
 
+    //TODO: enhance bot logic, it takes way too many cards and is poor all the time
+
     let requestBody = JSON.stringify({ takeCard: takeCard })
 
     //Send action request to API
-
     gameData = await sendRequest('https://koodipahkina.monad.fi/api/game/' + gameId + '/action', requestBody )
     console.log(gameData.status)
 
@@ -61,11 +67,15 @@ const checkSetCard = (cardsArray, nextCard) =>  {
     return takeCard
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 let gameData = await startGame()
 const gameId = gameData.gameId
 
 while(gameData.status.finished === false) {
-
+    await sleep(1000)
     gameData = await playRound(gameData, gameId)
     console.log(gameData)
 }
