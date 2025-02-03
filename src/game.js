2fc68fd52077c23aae3be15eb512e6a05ae1c760
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-02-03 16:29:26 +0200

    use array.some in chekSetCard() so we can return immediately if its a set card. in first round play, if high value card take it. And some style changes in code

--- src/game.js
+++ src/game.js
@@ -1,15 +1,15 @@
 import sendRequest from './requestService.js';
 
-async function startGame() {
 
+async function startGame() {
     let gameData = await sendRequest()
 
     return gameData
 }
 
+
 async function playTurn(gameData, gameId) {
-    
-    console.log(gameData.status)
+    //console.log(gameData.status)
     let takeCard = false
     let player = gameData.status.players.find(({ name }) => name === 'JJarvenpaa')
     let money = player.money
@@ -18,19 +18,20 @@ async function playTurn(gameData, gameId) {
     let nextCard = gameData.status.card
 
 
-    //TODO: idea to keep money situation good: if cardValue >= 12, take it. It's a good amount of money
-    // This way we can remove moneyBuffer, because I think that makes us go 0 money
+    //Game 8 is used on code without current changes
+    //Game 10 is used on current code
     if(money == 0) {
         takeCard = true;
         
     } else if(gameData.status.cardsLeft == 24 && cardValue >= 3) {
-        takeCard = firstRoundPlay(nextCard)
+        takeCard = firstRoundPlay(nextCard, cardValue)
 
     } else if(money >= cardValue && checkSetCard(cardsArray, nextCard)) {
         takeCard = true;
 
     } else if(money >= cardValue && money <= 7 && cardValue >= 3) {
         //take it to keep money situation good
+        //We take too many cards now, I think we need to implement some probability based decision here now
         takeCard = true
 
     } else {
@@ -39,39 +40,40 @@ async function playTurn(gameData, gameId) {
 
     //TODO: enhance bot logic, it takes way too many cards and is poor all the time
 
-    let requestBody = JSON.stringify({ takeCard: takeCard })
-
     //Send action request to API
+    let requestBody = JSON.stringify({ takeCard: takeCard })
     gameData = await sendRequest('https://koodipahkina.monad.fi/api/game/' + gameId + '/action', requestBody )
     console.log(gameData.status)
 
     return gameData
 }
 
-//if not set card and has money, take card if card is smaller or equal to than half of the highest card (36)
-const firstRoundPlay = (nextCard) => {
 
+//if not set card and has money, take card if card is smaller or equal to than half of the highest card (36)
+const firstRoundPlay = (nextCard, cardValue) => {
     let takeCard = false
 
-    if(nextCard <= 16) { takeCard = true }
+    //For testing only
+    //nextCard = 20
+    //cardValue = 12
+    if(nextCard <= 16 || cardValue >= 11) { takeCard = true }
 
     return takeCard
 }
 
-//check cardsArray and if one of the cards is offset 1 to the current card return true, otherwise false
-const checkSetCard = (cardsArray, nextCard) =>  {
-    let takeCard = false
 
-    if(cardsArray.length === 0) { return takeCard } 
+const checkSetCard = (cardsArray, nextCard) =>  {
+    if(cardsArray.length === 0) { return false } 
     //For testing only 
     //cardsArray = [25, 1, 3, 4]
-    //nextCard = 2
+    //nextCard = 24
     
-    cardsArray.forEach(currentCard => {
-        if(currentCard + 1 == nextCard || currentCard - 1 == nextCard) takeCard = true 
-    });
+    //check cardsArray and if one of the cards is offset 1 to the current card return true, otherwise false
+    const setCard = (card) => card + 1 == nextCard || card - 1 == nextCard
 
-    return takeCard
+    if(cardsArray.some(setCard)) return true
+   
+    return false
 }
 
 function sleep(ms) {
