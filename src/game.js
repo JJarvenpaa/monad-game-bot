30f7a6dfefbc1a5855dd206fb217034a5a982b9b
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-02-06 16:55:21 +0200

    Fix checkSetCard function by using a helper function in .some() in order for the OR condition to work

--- src/game.js
+++ src/game.js
@@ -15,18 +15,17 @@ async function playTurn(gameData, gameId) {
     let money = player.money
     let cardValue = gameData.status.money
     let cardsArray = player.cards
-    let nextCard = gameData.status.card
+    let tableCard = gameData.status.card
 
 
-    //Game 8 is used on code without current changes
-    //Game 10 is used on current code
+    //Game 11 is used on current code
     if(money == 0) {
         takeCard = true;
         
     } else if(gameData.status.cardsLeft == 24 && cardValue >= 3) {
-        takeCard = firstRoundPlay(nextCard, cardValue)
+        takeCard = firstRoundPlay(tableCard, cardValue)
 
-    } else if(money >= cardValue && checkSetCard(cardsArray, nextCard)) {
+    } else if(money >= cardValue && checkSetCard(cardsArray, tableCard)) {
         takeCard = true;
 
     } else if(money >= cardValue && money <= 7 && cardValue >= 3) {
@@ -50,30 +49,38 @@ async function playTurn(gameData, gameId) {
 
 
 //if not set card and has money, take card if card is smaller or equal to than half of the highest card (36)
-const firstRoundPlay = (nextCard, cardValue) => {
+const firstRoundPlay = (tableCard, cardValue) => {
     let takeCard = false
 
     //For testing only
-    //nextCard = 20
+    //tableCard = 20
     //cardValue = 12
-    if(nextCard <= 16 || cardValue >= 11) { takeCard = true }
+    if(tableCard <= 16 || cardValue >= 11) { takeCard = true }
 
     return takeCard
 }
 
 
-const checkSetCard = (cardsArray, nextCard) =>  {
+const checkSetCard = (cardsArray, tableCard) =>  {
     if(cardsArray.length === 0) { return false } 
     //For testing only 
     //cardsArray = [25, 1, 3, 4]
-    //nextCard = 24
+    //tableCard = 24
     
     //check cardsArray and if one of the cards is offset 1 to the current card return true, otherwise false
-    const setCard = (card) => card + 1 == nextCard || card - 1 == nextCard
+    function IsSetCard(card) {
+        if(card + 1 == tableCard || card - 1 == tableCard) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-    if(cardsArray.some(setCard)) return true
-   
-    return false
+    if(cardsArray.some(IsSetCard)) { 
+        return true 
+    } else {
+        return false
+    }
 }
 
 function sleep(ms) {
