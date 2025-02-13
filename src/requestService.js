0d86b6b327d2f196a4f88660c28564e216ae6d48
VeiJari <nope>
2025-02-13 13:39:27 +0200

    Add timeout to request and surround it in try catch. Handle error message from API and common Fetch API errors

--- src/requestService.js
+++ src/requestService.js
@@ -4,16 +4,50 @@ export default async function sendRequest(url = 'https://koodipahkina.monad.fi/a
     //TODO: how to point process.env to get .env from root
     if(process.env.TOKEN == undefined) throw new Error("No token found, remember to add it to .env file");
     
-    const response = await fetch(url, {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + process.env.TOKEN,
-        },
-        body: body,
-    });
-    //TODO: error handling
-    const data = await response.json()
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.TOKEN,
+            },
+            signal: AbortSignal.timeout(5000), 
+            body: body,
+        });
+        //TODO: error handling
+        const data = await response.json()
+        if(data.message != undefined) {
+            throw new Error('API returned error with message: ' + data.message)
+            //Handle API specific errors
+            //TODO: handle authorization missing
+            //TODO: handle wrong authorization
+            //TODO: handle game update without body
+            //TODO: handle game update with wrong data
+        } 
+        if(data.errors != undefined && data.errors.length > 0) {
+            let errors = ''
+            for(const error in data.errors) {
+                errors += error
+            }
 
-    return data
+            throw new Error('API returned errors: ' + errors)
+        }
+
+        return data
+    
+    } catch(err) {
+        switch(err.name) {
+            case 'TimeoutError':
+                throw new Error('Request timeout')
+
+            case 'AbortError':
+                throw new Error('Fetch aborted by user action')
+            
+            case 'TypeError':
+                throw new Error('TypeError encountered')
+            
+            default:
+                throw new Error(`type: ${err.name}, message: ${err.message}`)
+        }
+    }
 }
